401008a65048fe1b2b655bac4709a004eca9f021
aWildIvysaur <44836648+aWildIvysaur@users.noreply.github.com>
2026-01-31 22:39:34 +0000

    FEAT: paint UI
    Added UI where user can "paint" on a canvas

--- gui.py
+++ gui.py
@@ -3,25 +3,41 @@ Docstring for gui
 """
 
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtCore import Qt
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QPainter, QBrush, QPixmap, QMouseEvent
 
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("GUI")
+        self.setFixedSize(QSize(280, 280))
         self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(280, 280)
+        canvas = QPixmap(280, 280)
         canvas.fill(Qt.GlobalColor.white)
         self.label.setPixmap(canvas)
-        self.centralWidget()
-        self.draw_something()
+        self.setCentralWidget(self.label)
 
-    def draw_something(self):
+    def mouseMoveEvent(self, a0: QMouseEvent) -> None:
+        """Draw on canvas when mouse is moved while clicking"""
         canvas = self.label.pixmap()
-        painter = QtGui.QPainter(canvas)
-        painter.drawLine(10, 10, 10, 0)
+        painter = QPainter(canvas)
+        print(a0.buttons())
+        if a0.buttons() == Qt.MouseButton.LeftButton:
+            painter.setBrush(QBrush(Qt.BrushStyle.SolidPattern))
+            painter.setPen(Qt.GlobalColor.black)
+            painter.setBrush(Qt.GlobalColor.black)
+        else:
+
+            painter.setBrush(QBrush(Qt.BrushStyle.SolidPattern))
+            painter.setPen(Qt.GlobalColor.white)
+            painter.setBrush(Qt.GlobalColor.white)
+
+        x = int(a0.position().x())
+        y = int(a0.position().y())
+        painter.drawEllipse(x - 2, y - 2, 4, 4)
         painter.end()
         self.label.setPixmap(canvas)
 
--- model_trainer.py
+++ model_trainer.py
@@ -2,14 +2,12 @@
 Docstring for main
 """
 
+from typing import Any
 import numpy
+import keras
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-import keras.datasets
-from typing import Any
 from numpy.typing import NDArray
 
 (train_data, train_awns), (test_data, test_awns) = keras.datasets.mnist.load_data(
@@ -17,7 +15,7 @@ from numpy.typing import NDArray
 )
 
 
-def flatten_dataset(data: tuple[int, int]) -> NDArray[Any]:
+def flatten_data(data: tuple[int, int]) -> NDArray[Any]:
     """flattens 3D tuple into 2D numpy array"""
     data_array = numpy.asarray(data)
     samples, x, y = data_array.shape
@@ -29,13 +27,22 @@ def flatten_dataset(data: tuple[int, int]) -> NDArray[Any]:
 def test_mlp(
     data: NDArray[Any], awns: tuple[int], test_data: NDArray[Any], test_awns: tuple[int]
 ):
-    mlp = MLPClassifier(solver="sgd", hidden_layer_sizes=(10,), random_state=1)
+    """
+    Trains and tests 2 models using MLP classification with SGD and LBFGS solvers
+    """
+    # SGD MLP (0.3376)
+    mlp = MLPClassifier(
+        solver="sgd", hidden_layer_sizes=(10,), random_state=1, verbose=True
+    )
     mlp.fit(data, awns)
     mlp_output = mlp.predict(test_data)
     print("sgd")
     print(accuracy_score(test_awns, mlp_output))
 
-    mlp = MLPClassifier(solver="lbfgs", hidden_layer_sizes=(10,), random_state=1)
+    # LBFGS MLP
+    mlp = MLPClassifier(
+        solver="lbfgs", hidden_layer_sizes=(10,), random_state=1, verbose=True
+    )
     mlp.fit(data, awns)
     neural_output = mlp.predict(test_data)
     print("lbfgs")
@@ -45,20 +52,23 @@ def test_mlp(
 def test_forest(
     data: NDArray[Any], awns: tuple[int], test_data: NDArray[Any], test_awns: tuple[int]
 ):
-    forest = RandomForestClassifier(n_estimators=500)
+    """
+    Trains and tests 2 models using random forest classification with 500 and 5000 estimators
+    """
+    # 500 Forest (0.9712)
+    forest = RandomForestClassifier(n_estimators=500, n_jobs=6, verbose=True)
     forest = forest.fit(data, awns)
     forest_output = forest.predict(test_data)
     print("Random Forest with n_estimators:500")
     print(accuracy_score(test_awns, forest_output))
 
-    forest = RandomForestClassifier(n_estimators=5000)
+    # 5000 Forest (0.972)
+    forest = RandomForestClassifier(n_estimators=5000, n_jobs=6, verbose=True)
     forest = forest.fit(data, awns)
     forest_output = forest.predict(test_data)
     print("Random Forest with n_estimators:5000")
     print(accuracy_score(test_awns, forest_output))
 
 
-# test_mlp(flatten_dataset(train_data), train_awns, flatten_dataset(test_data), test_awns)
-test_forest(
-    flatten_dataset(train_data), train_awns, flatten_dataset(test_data), test_awns
-)
+# test_mlp(flatten_data(train_data), train_awns, flatten_data(test_data), test_awns)
+# test_forest(flatten_data(train_data), train_awns, flatten_data(test_data), test_awns)
